c51fda2468559113a9273e0567c900185e04a8e7
asfaquralam1 <ahsansagor449@gmail.com>
2022-08-19 19:38:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import styles from './styles'
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter} md:container md:mx-auto`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart} md:container md:mx-auto`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart} md:container md:mx-auto`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+        </div>
+      </div>
+    </div>
+  )
+
+export default App
---
+++ src/components/CTA.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles'
+import Button from './Button'
+
+const Cta = () => {
+  return (
+    <div>Cta</div>
+  )
+}
+
+export default Cta
